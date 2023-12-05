5850b141b385447e3ef1808a9e8d853d9dd10b40
Master <nghiemxthai@gmail.com>
2023-12-05 15:13:16 -0800

    fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,18 +13,12 @@ import ContactPage from './pages/Contact/contact.jsx'
 const router = createBrowserRouter([
   {
     path: '/',
-    // main layout (wraps the remaining page components)
     element: <App />,
-    // https://reactrouter.com/en/main/route/error-element#errorelement
-    // When exceptions are thrown in loaders, actions, or component rendering, instead of the normal render path for your Routes (<Route element>), the error path will be rendered (<Route errorElement>) and the error made available with useRouteError.
+    
     errorElement: <ErrorPage />,
-    // An array of Route objects with nested routes on the children property.
+  
     children: [
-      // https://reactrouter.com/en/main/route/route
-      // Routes are perhaps the most important part of a React Router app. They couple URL segments to components, data loading and data mutations. Through route nesting, complex application layouts and data dependencies become simple and declarative.
       {
-        // https://reactrouter.com/en/main/route/route#index
-        // Determines if the route is an index route. Index routes render into their parent's
         index: true,
         element: <HomePage />,
       }
