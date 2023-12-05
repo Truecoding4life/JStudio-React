0205e03f604139f1c7de9cb4b769bbc3906fbf45
Master <nghiemxthai@gmail.com>
2023-12-04 23:30:07 -0800

    added Resume Page

--- Jay-Studio/src/pages/resume/resume.jsx
+++ Jay-Studio/src/pages/resume/resume.jsx
@@ -1,7 +1,16 @@
 export default function ResumePage() {
     return(
-        <div className="container">
-            
+        <div className="container mt-5">
+            <h1> Resume </h1>
+            <h4> Computer Science Skills</h4>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">Pseudocode</li>
+                <li className="list-group-item">HTML5</li>
+                <li className="list-group-item">CSS3</li>
+                <li className="list-group-item">Java Script and React</li>
+                <li className="list-group-item">MongoDb and IndexDB</li>
+            </ul>
+            <a href="https://docs.google.com/document/d/1nRdWUr0BJAV-i-XUV5W1z-jJe613ZwSLYrtNlvE0BuY/edit?usp=sharing" className="btn bg-primary mt-3 text-white"> Download my resume</a>
         </div>
     )
 }
