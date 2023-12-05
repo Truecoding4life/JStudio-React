37f6a8579c3c94e95e82a4fad02e292d8efec99d
Master <nghiemxthai@gmail.com>
2023-12-04 23:45:34 -0800

    added resume and footer link

--- Jay-Studio/src/components/footer/Footer.jsx
+++ Jay-Studio/src/components/footer/Footer.jsx
@@ -5,6 +5,22 @@ export default function Footer(){
         <div className='footer'>
             <h6> Design by JStudio. 2023</h6>
             <p> Make Your Website Everyday Better </p>
+            <div className="col-6">
+                        <ul className="contact-info">
+
+                            <li >
+                            Send me an Email: <a href="mailto: nghiemxthai@gmail.com.com"> Click Here </a>
+                            </li>
+                            <li>
+                                Github Repositories: <a href="https://github.com/Truecoding4life/challenge2"> Click
+                                    Here</a>
+                            </li>
+                            <li >
+                                Linked In: <a href="https://www.linkedin.com/in/thai-nghiem-319292267/"> Click Here</a>
+                            </li>
+                        </ul>
+                    </div>
         </div>
+        
     )
 }
