24cddb5d0542beb04c6b65aa5ebf12e81b9a45b9
Master <nghiemxthai@gmail.com>
2023-12-08 18:24:53 -0800

    added icon to navbar

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ export default function HomePage() {
     return (
 
         <div className="container">
-            
+
             <div className="row">
                 <div className="col-12 col-md-6 align-self-center home-header">
                     <image className="center">
@@ -20,11 +20,11 @@ export default function HomePage() {
                 <div className="col-12 col-md-6 align-self-center home-header">
                     <div className="col-12 text-center">
                         <h1 id="welcome-quote"> Build your website everyday better</h1>
-                        <h4> Jay Studio aim to deliver</h4>
-                        <p> Website for Personal or Business use </p>
-                        <p> Modern and Minimal Design</p>
+                        <h4> Turn your idea to reality</h4>
+                        <p> We design website for business and personal </p>
+                        <p> We Focus on Modern and Minimal Design</p>
                         <p> Fast and Optimize Running Time </p>
-                        <p> Secure User Information </p>
+
                    
                     </div>
                 </div>
