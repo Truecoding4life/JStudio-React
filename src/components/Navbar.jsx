9e2be65fe097e26a277cf870b27b89fd9b6eccee
Master <nghiemxthai@gmail.com>
2023-12-08 16:56:51 -0800

    added logo and font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
+import image from '../assets/logo.png';
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-sm ">
       <div className="container-fluid ">
         <Link className="navbar-brand" to="/">
+          <image>
+            <img
+              src={image}
+              alt="logo"
+              width="50px"
+            />
+          </image>
           J Studio
         </Link>
         <ul className="nav justify-content-center">
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,23 @@
+import logo from '../assets/logo.png';
+
 export default function HomePage() {
     return (
+
         <div className="container">
+            
             <div className="row">
-                <div className="col-12 align-self-center home-header">
+                <div className="col-12 col-md-6 align-self-center home-header">
+                    <image className="center">
+                        <img
+                            src={logo}
+                            alt="logo"
+                            id="homepage-logo"
+                        />
+                    </image>
+                </div>
+
+
+                <div className="col-12 col-md-6 align-self-center home-header">
                     <div className="col-12 text-center">
                         <h1 id="welcome-quote"> Build your website everyday better</h1>
                         <h4> Jay Studio aim to deliver</h4>
@@ -13,6 +28,8 @@ export default function HomePage() {
                    
                     </div>
                 </div>
+
+
 
             </div>
         </div>
