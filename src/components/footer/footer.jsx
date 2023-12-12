b46ec14949b5490d5e98fdc75ecfd26c396730d8
Master <nghiemxthai@gmail.com>
2023-12-11 23:02:17 -0800

    fixed github link

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -23,7 +23,7 @@ export default function Footer() {
             </a>
           </li>
           <li>
-            <a href="https://github.com/Truecoding4life/challenge2">
+            <a href="https://github.com/Truecoding4life">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="30"
