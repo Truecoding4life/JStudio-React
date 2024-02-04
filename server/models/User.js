8b35fc6ded0d2fd6cf222fd669096ddb67f2a87f
Master <nghiemxthai@gmail.com>
2024-02-03 18:44:53 -0800

    added user models

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+const {Schema, model} = require('mongoose');
+const timeFormat = require('../utils/timeFormat');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+    messages: [
+        {
+            message: {
+                type: String,
+                required: 'Leave a message!',
+                trim: true,
+                minlength: 1,
+                maxlength: 280,
+            },
+            email: {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            name : {
+                type: String,
+                required: true,
+                trim: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now,
+                get: (timestamp) => dateFormat(timestamp),
+            },
+        },
+    ],
+});
+
+const User = model('User', userSchema);
+module.exports = User;
