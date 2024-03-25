9ba6d71fb3f8965cede6af3b1c4f17f70d78a333
Master <nghiemxthai@gmail.com>
2024-03-24 17:03:13 -0700

    splited about section card to it own component

--- client/src/containers/About/About.jsx
+++ client/src/containers/About/About.jsx
@@ -1,16 +1,16 @@
-import photo1 from "../../assets/images/website/about.jpg";
+import photo1 from "../../assets/images/website/logo.png";
 import photo2 from "../../assets/images/website/photo2.jpg";
 import photo3 from "../../assets/images/website/photo3.png";
-import {  useState } from "react";
+import { useState } from "react";
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import './style.css'
 import { Button } from "react-bootstrap";
-import CarouselHelper from "./caroselHelper";
+import CarouselCard from "./Card";
 
 
 export default function AboutPage() {
- 
+
 
   const [aboutIndex, setAboutIndex] = useState(0)
 
@@ -30,7 +30,7 @@ export default function AboutPage() {
     },
     {
       title: "User Experience",
-      text: "To me, great user experience is paramount, I strive to create a lasting impact through innovative solutions.",
+      text: "To me, great user experience is paramount, I strive to create a lasting impact through great user experience.",
       image: photo3
     }
   ]
@@ -41,54 +41,22 @@ export default function AboutPage() {
 
   return (
     <section id="about-container" className="container-fluid">
-      <div className="">
+      <div>
         <div className="section-about d-flex align-items-center animate__animated animate__fadeIn">
           <div className="button-about-div">
             <Button type="button" className="carousel-button" onClick={() => { aboutIndex > 0 && setAboutIndex(aboutIndex - 1) }}>
               <ChevronLeftIcon fontSize="large" />
             </Button>
           </div>
-          <div className="row ">
+         
 
-            <div  className="col-12 col-sm-6 col-md-6 col-lg-4 pt-5 photograph  text-center">
-              <image>
-                <img key={aboutIndex} src={image} alt="logo" width="100%" className="about-photo align-items-center animate__animated animate__fadeInRight" style={{ borderRadius: 5 }} />
-              </image>
-            </div>
-            <div  className="col-12 col-sm-6 col-md-6 col-lg-8 align-self-center about-me ">
-              <div key={aboutIndex} className="col-12 align-items-center animate__animated animate__fadeInRight">
-                  <h3 className="about-title  "> {title}  </h3>
-                <p className="about-text">
-                  {" "}
-                  {text}
-                </p>
-              </div>
-            </div>
-            <div className="col-12 carousel">
-              <CarouselHelper index={aboutIndex} />
-            </div>
-
-
-
-
-          </div>
-
+          <CarouselCard title={title} text={text} image={image} aboutIndex={aboutIndex} />
           <div className="button-about-div">
             <Button type="button" className="carousel-button" onClick={() => { aboutIndex < aboutData.length - 1 && setAboutIndex(aboutIndex + 1) }}>
               <NavigateNextIcon fontSize="large" />
             </Button>
           </div>
         </div>
-
-
-
-
-
-
-
-
-
-
       </div>
     </section>
   );
