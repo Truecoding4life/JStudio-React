48ae151c6772dd1ca9f88e400b4ea72a0cdb64f7
Master <nghiemxthai@gmail.com>
2024-03-30 19:14:45 -0700

    changed 404 page

--- client/src/containers/noMatch/NoMatch.jsx
+++ client/src/containers/noMatch/NoMatch.jsx
@@ -1,25 +1,25 @@
 import "./style.css";
 import errorGif from "./noMatch.gif";
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { Container } from "@mui/material";
 
 function NoMatch() {
   const location = useLocation();
   const error = location.state && location.state.error;
   
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-      }}
+      <div   id='no-found-container' className="container-fluid d-flex align-items-center justify-content-center  error-container"  >
+ <Container
+     style={{height:'100vh'}}
+    className="w-100 d-flex flex-column justify-content-center align-items-center"
+  
     >
-      <div>
-      <img  src={errorGif} alt="ATAT 404 error gif" />
-      </div>
+   
+
+   <Link to="/" className="btn home-button" >Go home page</Link>     
+    </Container>
     </div>
+   
   );
 }
 
