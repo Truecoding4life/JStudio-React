cde224343886e13ce5424af271abe89dfa4370da
Master <nghiemxthai@gmail.com>
2024-04-03 08:39:25 -0700

    make timestamp modular and functional until the next day

--- client/src/components/Navbar/MessageCard.jsx
+++ client/src/components/Navbar/MessageCard.jsx
@@ -15,51 +15,8 @@ import * as dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat';
-
+import timeStampSmart from '../../ulti/timestamp'
 dayjs.extend(LocalizedFormat)
-
-function monthToInt(sentMonth){
-  switch (sentMonth) {
-    case 'Jan':
-      sentMonth = 1
-      break;
-    case 'Feb':
-      sentMonth = 2
-      break;
-    case 'Mar':
-      sentMonth = 3
-      break;
-    case 'Apr':
-      sentMonth = 4
-      break;
-    case 'May':
-      sentMonth = 5
-      break;
-    case 'Jun':
-      sentMonth = 6
-      break;
-    case 'Jul':
-      sentMonth = 7
-      break;
-    case 'Aug':
-      sentMonth = 8
-      break;
-    case 'Sep':
-      sentMonth = 9
-      break;
-    case 'Oct':
-      sentMonth = 10
-      break;
-    case 'Nov':
-      sentMonth = 11
-      break;
-    case 'Dec':
-      sentMonth = 12
-      break;
-  }
-  return sentMonth;
-}
-
 
 
 export default function RecipeReviewCard({ name, date, text, id, email }) {
@@ -68,37 +25,6 @@ export default function RecipeReviewCard({ name, date, text, id, email }) {
   });
   date = date.trim();
   const currentFullDate = dayjs(dayjs().locale('en').toISOString()).format('LLL').split(' ');
-  
- 
-  let sentYear = parseInt(date.split(" ")[2]);
-  // tested
-  const sentDate = parseInt(date.split(" ")[1].split('t')[0]); // 12
-  let sentMonth = monthToInt(date.split(" ")[0]); // 3
-  let sentHour = parseInt(date.split(" ")[4][0]); // 2
-  let sentTime = date.split(" ")[5] // am
-  let sentMin = parseInt(date.split(" ")[4].split(':')[1]); // 39
-  const currentDate = parseInt(currentFullDate[1]);
-  let currentMonth = monthToInt(currentFullDate[0].slice(0,3))
-  let currentTime = currentFullDate[4].toLowerCase();
-  let currentHour = parseInt(currentFullDate[3].split(":")[0])
-  let currentMin = currentFullDate[3].split(":")[1]
-//
-  console.log('currentFullDate', currentFullDate);
- 
-
-  
-
-  // if current month and data is the same then return the different hour if the hour are the same then return the min
-  const handleTimeStamp = () => {
-    if (currentDate === sentDate && currentMonth === sentMonth && currentHour === sentHour && currentTime === sentTime) {
-      return `Received ${currentMin - sentMin}  Minutes ago`;
-    }
-    else if (currentDate === sentDate && currentMonth === sentMonth && currentHour !== sentHour)  {
-      return `Received ${currentHour - sentHour} Hours ago`;
-    }
-    // // Return a default value (e.g., 0) if the conditions are not met
-    return date;
-  }
 
   const handleRemove = async () => {
     try {
@@ -138,7 +64,7 @@ export default function RecipeReviewCard({ name, date, text, id, email }) {
         }
         subheader={
           <div >
-            <Typography style={{ fontFamily: 'Nunito Sans' }} variant="inherit">{handleTimeStamp()}</Typography>
+            <Typography style={{ fontFamily: 'Nunito Sans' }} variant="inherit">{timeStampSmart(date, currentFullDate)}</Typography>
           </div>
         }
 
