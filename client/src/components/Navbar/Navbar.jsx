ec8091062d2d9967d7ec1d570e7c4e2f36c9ab8e
Master <nghiemxthai@gmail.com>
2024-04-04 17:47:10 -0700

    changes navbrand color

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -56,7 +56,7 @@ export default function NavbarLi() {
     <Navbar expand="md">
       <Navbar.Brand
         href="/"
-        className="navbar-brand gradient-text"
+        className="navbar-brand nav-brand-color"
         style={{ color: '#4ec6a2f1' }}
       >
         J Studio
--- client/src/containers/Home/Intro.jsx
+++ client/src/containers/Home/Intro.jsx
@@ -9,7 +9,7 @@ import video from '../../assets/video/intro.mp4';
 
     return (
       <div>
-        <video style={{height:'100vh', width:'100%', backgroundColor:'black'}}    autoPlay muted onEnded={handleVideoEnd}>
+        <video style={{ height:'100vh', width:'100%', backgroundColor:'black', objectFit:'cover'}}    autoPlay muted onEnded={handleVideoEnd}>
           <source src={video} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
