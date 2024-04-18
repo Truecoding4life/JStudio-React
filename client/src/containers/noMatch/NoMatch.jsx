336a3a9f7170b0cf6ba3b4642bd376a9093298df
Master <nghiemxthai@gmail.com>
2024-04-18 02:44:30 -0700

    deleted no match gif import

--- client/src/containers/noMatch/NoMatch.jsx
+++ client/src/containers/noMatch/NoMatch.jsx
@@ -1,25 +1,24 @@
 import "./style.css";
-import errorGif from "./noMatch.webp";
 import { Link, useLocation } from 'react-router-dom';
 import { Container } from "@mui/material";
 
 function NoMatch() {
   const location = useLocation();
   const error = location.state && location.state.error;
-  
-  return (
-      <div   id='no-found-container' className="container-fluid d-flex align-items-center justify-content-center  error-container"  >
- <Container
-     style={{height:'100vh'}}
-    className="w-100 d-flex flex-column justify-content-center align-items-center"
-  
-    >
-   
 
-   <Link to="/" className="btn home-button" >Go home page</Link>     
-    </Container>
+  return (
+    <div id='no-found-container' className="container-fluid d-flex align-items-center justify-content-center  error-container"  >
+      <Container
+        style={{ height: '100vh' }}
+        className="w-100 d-flex flex-column justify-content-center align-items-center"
+
+      >
+
+
+        <Link to="/" className="btn home-button" >Go home page</Link>
+      </Container>
     </div>
-   
+
   );
 }
 
